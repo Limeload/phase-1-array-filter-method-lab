0906f01341ba7653553806350af3a031fe6ed029
Shraddha Rao <shraddharaom@gmail.com>
2022-11-22 08:19:32 -0600

    commit tree fabbc9908015d48c9cba370ec6cb02137f821e8c

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    let matching = drivers.filter((driver) => 
+     driver === name.toUpperCase() ||
+     driver === name ||
+     driver === name.toLowerCase()
+    )
+    return matching;
+};
+
+function fuzzyMatch(drivers, name) {
+    let providedLetters = drivers.filter((driver) =>
+    driver.substring(0,2) === name.substring(0,2)
+    )
+    return providedLetters;
+};
+
+function matchName(drivers, name) {
+    let match = drivers.filter((driver) => 
+    driver.name === name
+    )
+    return match;
+}
